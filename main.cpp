ab9003483ed8aaacdbd1b5c8e991703ee5424aad
Katolikov <katolikovart@gmail.com>
2020-05-21 20:32:58 +0300

    добавил шаблон для дальнешей работы

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
-#include <iostream> 
+#include <iostream>
+#include <fstream>
+#include <chrono>
 
-int main(){
+int main(int argc, char* argv[]){
 
-	std::cout << "Hello\n";
+    std::fstream binaryFile;
+    binaryFile.open(argv[1], std::ios::binary);
+
+    auto startFirst = std::chrono::system_clock::now();
+
+    //Some check function
+
+    auto endFirst = std::chrono::system_clock::now();
+
+    auto startSecond = std::chrono::system_clock::now();
+
+    //Some check function
+
+    auto endSecond= std::chrono::system_clock::now();
+
+    std::chrono::duration<double> firstTime = endFirst-startFirst;
+    std::chrono::duration<double> secondTime = endSecond-startSecond;
+
+    binaryFile.close();
+
+    std::cout << "First check: " << firstTime.count() << "\n";
+    std::cout << "Second check: " << secondTime.count() << "\n";
+
 	return 0;
-
 }
