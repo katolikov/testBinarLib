decbd826ea7587a687dc9175b3d0a36fc4de6695
Katolikov <katolikovart@gmail.com>
2020-05-21 21:11:42 +0300

    added tester for ifstream

--- main.cpp
+++ main.cpp
@@ -4,12 +4,21 @@
 
 int main(int argc, char* argv[]){
 
-    std::fstream binaryFile;
-    binaryFile.open(argv[1], std::ios::binary);
+    size_t size = 1000000;
+    char buffer[size];
+
+    std::cout << "Test READ FROM FILE: \nFILE SIZE: 10GB\n";
+
+    std::ifstream binaryFile (argv[1], std::ios::in | std::ios::binary);
+
+    if(!binaryFile){
+        std::cerr << "Cannot open file!\n";
+        return 1;
+    }
 
     auto startFirst = std::chrono::system_clock::now();
 
-    //Some check function
+    binaryFile.read(buffer, size); //check first
 
     auto endFirst = std::chrono::system_clock::now();
 
@@ -25,6 +34,7 @@ int main(int argc, char* argv[]){
     binaryFile.close();
 
     std::cout << "First check: " << firstTime.count() << "\n";
+
     std::cout << "Second check: " << secondTime.count() << "\n";
 
 	return 0;
