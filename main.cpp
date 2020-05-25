5ec8d11a81bc5084ccc52df9cd669f8fc816c0e0
Katolikov <katolikovart@gmail.com>
2020-05-25 22:38:01 +0300

    add msgpack test

--- main.cpp
+++ main.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <msgpack.hpp>
 
 int main(int argc, char* argv[]){
 
-    size_t size = 1000000;
-    char buffer[size];
-
-    std::cout << "Test READ FROM FILE: \nFILE SIZE: 10GB\n";
+    std::cout << "Test READ FROM FILE: \nFILE SIZE: 1GB\n";
 
     std::ifstream binaryFileIn (argv[1], std::ios::in | std::ios::binary);
 
-    std::ofstream binaryFileOut (argv[1], std::ios::in | std::ios::binary);
+    std::string buffer((std::istreambuf_iterator<char>(binaryFileIn)), std::istreambuf_iterator<char>());
+
+    //std::ofstream binaryFileOut (argv[1], std::ios::in | std::ios::binary);
 
     if(!binaryFileIn){
         std::cerr << "Cannot open file!\n";
         return 1;
     }
 
+    msgpack::unpacked upd;
+
+    std::size_t offset = 0;
+
     auto startFirstRead = std::chrono::system_clock::now();
 
-    binaryFileIn.read(buffer, size); //check first
+    msgpack::unpack(upd, buffer.data(), buffer.size(), offset);
 
     auto endFirstRead = std::chrono::system_clock::now();
 
 
     auto startFirstWrite = std::chrono::system_clock::now();
 
-    binaryFileOut.write(buffer, size); //check first
+    //Some check function
 
     auto endFirstWrite = std::chrono::system_clock::now();
 
@@ -38,18 +42,18 @@ int main(int argc, char* argv[]){
     auto endSecond= std::chrono::system_clock::now();
 
     std::chrono::duration<double> firstTimeRead = endFirstRead-startFirstRead;
-    std::chrono::duration<double> firstTimeWrite = endFirstWrite-startFirstWrite;
 
-    std::chrono::duration<double> secondTime = endSecond-startSecond;
+    //std::chrono::duration<double> firstTimeWrite = endFirstWrite-startFirstWrite;
+
+    //std::chrono::duration<double> secondTime = endSecond-startSecond;
 
     binaryFileIn.close();
-    binaryFileOut.close();
+    //binaryFileOut.close();
 
     std::cout << "First check READ: " << firstTimeRead.count() << "\n";
 
-    std::cout << "First check WRITE: " << firstTimeWrite.count() << "\n";
+    //std::cout << "First check WRITE: " << firstTimeWrite.count() << "\n";
 
     //std::cout << "Second check: " << secondTime.count() << "\n";
-
-	return 0;
+    return 0;
 }
